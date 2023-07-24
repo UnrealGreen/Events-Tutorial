8356e2c92007e00e151f4f3e820c3db4c8fc39e5
UnrealGreen <thatrandomblackdude@gmail.com>
2023-07-24 15:42:25 -0500

    Add files via upload

---
+++ C# Events Learning/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿ namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Video youtube = new Video();
+            youtube.Title = "Unreal Engine Combat System Tutorial";
+
+            VideoEncoder youtubeEncoder = new VideoEncoder(); //publisher
+            MailService mailService = new MailService();  //SubScriber
+
+           youtubeEncoder.Encode(youtube);
+        }
+
+    }
+
+    public class MailService
+    {
+        public void OnVideoEncoded(object source, EventArgs e)
+        {
+            Console.WriteLine("MailService: Sending an email....");
+        }
+    }
+
+}
---
+++ C# Events Learning/ConsoleApp1/VideoEncoder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class VideoEncoder
+    {
+        //1. -Define a delegate. This class (VideoEncoder) is the publisher of the event
+        //2. - Define an Event based on delegate
+        //3. - Raise the Event
+
+        public delegate void VideoEncodedEventHandler(object source, EventArgs args); // Created a delegate
+
+        public event VideoEncodedEventHandler VideoEncoded; //Defined an event based on delegate
+         
+
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding Video,,,");
+            Thread.Sleep(3000);
+
+            OnVideoEncoded();
+
+        }
+
+        protected virtual void OnVideoEncoded() //Created a method for raising the event 
+        {
+            if (VideoEncoded != null)
+            {
+                VideoEncoded(this, EventArgs.Empty);
+            }
+
+        }
+    }
+}
